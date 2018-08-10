c653dbf8d2893b93e18cc9a931bed7dda6cd7d99
Aaron Metz <acmetz@gmail.com>
2018-08-09 22:38:38 -0400

    added button creation and messed with events

--- src/firefox/trackbutton.js
+++ src/firefox/trackbutton.js
@@ -1,1 +1,16 @@
-var loc = document.querySelector("div.subnav");
+function loadUp(){
+    var loc = document.querySelector("div.subnav>div.subnav-links");
+    console.log("loading here");
+    var link = loc.querySelector("a.subnav-item").cloneNode();
+    link.innerText = "Track Issues";
+    link.setAttribute("href","");
+    link.addEventListener("click",function(){console.log("clicked here")});
+    link.removeAttribute("data-selected-links");
+    link.setAttribute("id","issuetracker");
+    console.log(loc.querySelector("a#issuetracker"));
+    if(loc.querySelector("a[name=issuetracker]") == null)
+    {
+        loc.appendChild(link);
+    }
+}
+loadUp();
