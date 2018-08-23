d5fd33b45ca6a88db8f17b5fced36bc5e61169cb
Aaron Metz <acmetz@gmail.com>
2018-08-22 21:36:09 -0400

    got button working correctly

--- src/firefox/trackbutton.js
+++ src/firefox/trackbutton.js
@@ -1,14 +1,43 @@
-function loadUp(){
-    var loc = document.querySelector("div.subnav>div.subnav-links");
-    var link = loc.querySelector("a.subnav-item").cloneNode();
-    link.innerText = "Track Issues";
-    link.setAttribute("href","");
-    link.addEventListener("click",function(){console.log("clicked here")});
-    link.removeAttribute("data-selected-links");
-    link.setAttribute("id","issuetracker");
-    if(loc.querySelector("a[name=issuetracker]") == null)
-    {
-        loc.appendChild(link);
+const untrackedText = "Track Issues";
+const trackedText = "Stop Tracking";
+
+function createButton()
+{
+    var btn = containsButton();
+    if(!btn){
+        var loc = document.querySelector("div.subnav>div.subnav-links");
+        btn = loc.querySelector("a.subnav-item").cloneNode();
+        btn.setAttribute("href","");
+        btn.innerText = untrackedText;
+        btn.addEventListener("click", function(e){       
+            setTracked(this);
+            e.preventDefault();
+        });
+        btn.removeAttribute("data-selected-links");
+        btn.setAttribute("id","issuetracker");
+        if(loc.querySelector("a[name=issuetracker]") == null)
+        {
+            loc.appendChild(btn);
+        }
     }
+    return btn;
 }
-loadUp();
+
+function setTracked(link){
+    link.classList.toggle("selected");
+    link.innerText = link.innerText === untrackedText ? trackedText : untrackedText;          
+}
+
+function containsButton(){
+    var loc = document.querySelector("div.subnav > div.subnav-links").children;
+    for(var i=0;i<loc.length;i++){
+        element = loc[i];
+        if(element.innerText === untrackedText 
+            || element.innerText === trackedText){
+                return element;
+            }
+    }
+    return null;
+}
+
+createButton();
